3d691ec1319db0ced148ece8861106648acc32bd
Florent BOURASSE <fbourasse@jahia.com>
2026-06-10 11:39:11 +0200

    fix: update requiredPermission to module-specific permission in frontend registration
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- src/javascript/VersionsCleaner/register.jsx
+++ src/javascript/VersionsCleaner/register.jsx
@@ -7,20 +7,20 @@ export default () => {
     console.debug('%c versions-cleaner: activation in progress', 'color: #463CBA');
     registry.add('adminRoute', 'versionsCleaner', {
         targets: ['administration-server-systemHealth:999'],
-        requiredPermission: 'admin',
+        requiredPermission: 'versionsCleanerAdmin',
         label: 'versions-cleaner:label.menu_entry',
         isSelectable: false
     });
     registry.add('adminRoute', 'versionsCleanerExecution', {
         targets: ['administration-server-versionsCleaner:1'],
-        requiredPermission: 'admin',
+        requiredPermission: 'versionsCleanerAdmin',
         label: 'versions-cleaner:label.menu_execution',
         isSelectable: true,
         render: () => React.createElement(VersionsCleanerAdmin)
     });
     registry.add('adminRoute', 'versionsCleanerConfiguration', {
         targets: ['administration-server-versionsCleaner:2'],
-        requiredPermission: 'admin',
+        requiredPermission: 'versionsCleanerAdmin',
         label: 'versions-cleaner:label.menu_configuration',
         isSelectable: true,
         render: () => React.createElement(SchedulerConfig)
